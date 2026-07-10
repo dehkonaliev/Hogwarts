ac3e6599451da4dcd5e61856ec6839c385840080
Maqsudbek <dehkanaliev@gmail.com>
2026-07-10 19:35:56 +0500

    Student models yaratildi!

--- student/models.py
+++ student/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from home.models import CustomUser
+from teacher.models import Homework, Lesson
 
-# Create your models here.
+class StudentSubmit(models.Model):
+    homework = models.ForeignKey(Homework, on_delete=models.CASCADE)
+    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='submissions/')
+    xp_earned = models.PositiveIntegerField(default=0)
+    feedback = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.student.first_name
+    
+class StudentInfo(models.Model):
+    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE, limit_choices_to={'role': 'STUDENT'})
+    xp = models.PositiveIntegerField(default=0)
+    coins = models.PositiveIntegerField(default=0)
+    balance = models.DecimalField(max_digits=15, decimal_places=2)
+    
+    def __str__(self):
+        return self.student.first_name
+    
