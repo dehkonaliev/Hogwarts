7c6958671b9348dbfd5bd939ea8c06c63d5f9a3a
Maqsudbek <dehkanaliev@gmail.com>
2026-07-11 00:01:39 +0500

    Search and create function qo'shildi!

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import no_access
+from .views import no_access, home_page
 
 urlpatterns = [
+    path('', home_page, name='home'),
     path('no-access', no_access, name='no-access')
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_page(request):
+    return render(request, 'manager/mg-home.html')
+
+
 def no_access(request):
     return render(request, 'utils/no-access.html')
