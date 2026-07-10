d055ad275a0d81480179be014dc7be04d102c1df
Maqsudbek <dehkanaliev@gmail.com>
2026-07-10 19:26:54 +0500

    Teacher modellari yaratildi

--- teacher/models.py
+++ teacher/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from home.models import CustomUser
+from manager.models import Group
 
-# Create your models here.
+class Lesson(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    
+class Attendance(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE, limit_choices_to={'role': 'STUDENT'})
+    is_present = models.BooleanField(default=False)
+    
+    class Meta:
+        unique_together = ['lesson', 'student']
+        
+class Homework(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    file = models.FileField(upload_to='homework_materials/', blank=True, null=True)
+    image = models.ImageField(upload_to='homework_images/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
+    
